4ae99dfd006454ff7fb17f64bbad72d6373a516b
yusoojeong <yusj1231@gmail.com>
2020-09-23 15:19:04 +0900

    Code | Crolling nutrient

--- nutrient.py
+++ nutrient.py
@@ -4,9 +4,30 @@ from bs4 import BeautifulSoup
 def search_nutrient(menu):
     ## HTTP GET Request
     req = requests.get(f'https://terms.naver.com/search.nhn?query={menu}+%EC%98%81%EC%96%91%EC%84%B1%EB%B6%84&searchType=text&dicType=&subject=')
-    ## HTML 소스 가져오기
-    html = req.text
+
     ## BeautifulSoup으로 html소스를 python객체로 변환하기
-    ## 첫 인자는 html소스코드, 두 번째 인자는 어떤 parser를 이용할지 명시.
-    ## 이 글에서는 Python 내장 html.parser를 이용했다.
-    soup = BeautifulSoup(html, 'html.parser')
+    res = BeautifulSoup(req.text, 'html.parser')
+    data_list = res.select(
+        '#content > .search_result_area'
+    )
+
+    for data in data_list:
+        title = data.select(
+            '.info_area > .subject > .title > a > strong'
+        )[0].text
+        if title == f'{menu} 영양성분':
+            nutrient_list = str(data.select(
+                'p'
+            )[0].text).replace('|', '-').replace(' ', '').replace(':', '').split('-')
+            print(nutrient_list)
+            res_col = data.select(
+                ' .related > .info > .data'
+            )[2].text
+            print(res_col)
+            break
+
+    print("=================")
+    
+
+search_nutrient('비빔밥')
+search_nutrient('잔치국수')
